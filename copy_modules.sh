4597def3116f81c4a36dfd9151f7c94bf34f2180
AdiPat <adithemagnificent@gmail.com>
2012-04-06 13:44:36 +0530

    Make default READAHEAD size a kernel parameter.
    Add new kernel parameter "readahead", which would be used instead of the
    value of VM_MAX_READAHEAD. If the parameter is not specified, the default
    of 128kb would be used.

--- copy_modules.sh
+++ copy_modules.sh
@@ -1,30 +1,30 @@
 cd /home/aditya/i9003/Modules/
 
-cp pcbc.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/crypto/pcbc.ko
+cp pcbc.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/crypto/pcbc.ko
 
-cp st_drv.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/drivers/misc/ti-st/st_drv.ko
+cp st_drv.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/drivers/misc/ti-st/st_drv.ko
 
-cp scsi_wait_scan.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/drivers/scsi/scsi_wait_scan.ko 
+cp scsi_wait_scan.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/drivers/scsi/scsi_wait_scan.ko 
 
-cp btwilink.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/drivers/bluetooth/btwilink.ko
+cp btwilink.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/drivers/bluetooth/btwilink.ko
 
-cp samsung_battery.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/battery/samsung_battery.ko
+cp samsung_battery.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/battery/samsung_battery.ko
 
-cp Si4709_driver.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/Si4709_driver.ko
+cp Si4709_driver.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/Si4709_driver.ko
 
-cp vibetonz.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/vibetonz/vibetonz.ko
+cp vibetonz.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/vibetonz/vibetonz.ko
 
-cp param.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/param/param.ko
+cp param.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/param/param.ko
 
-cp yas_mag_kernel_driver.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/yas529/yas_mag_kernel_driver.ko
+cp yas_mag_kernel_driver.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/yas529/yas_mag_kernel_driver.ko
 
-cp yas_acc_kernel_driver.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/bma222/yas_acc_kernel_driver.ko
+cp yas_acc_kernel_driver.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/bma222/yas_acc_kernel_driver.ko
 
-cp gp2a.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/gp2a/gp2a.ko  
+cp gp2a.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/gp2a/gp2a.ko  
 
-cp yas_ori_kernel_driver.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/orientation/yas_ori_kernel_driver.ko
+cp yas_ori_kernel_driver.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/orientation/yas_ori_kernel_driver.ko
 
-cp gps.ko /home/aditya/i9003/ramdisk/lib/modules/2.6.35.7/kernel/samsung/gps/gps.ko
+cp gps.ko /home/aditya/i9003/urt/ramdisk/lib/modules/2.6.35.7/kernel/samsung/gps/gps.ko
 
 echo "Copied Modules..."
 

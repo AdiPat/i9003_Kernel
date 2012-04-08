e271218e63c85c82d5643b634ec2682d090a86f9
AdiPat <adithemagnificent@gmail.com>
2012-04-08 20:45:14 +0530

    Overclock 1.2Ghz with OPP sysfs feature.
    i9003 OMAP 3630 Overclocked to 1.2 Ghz at OPP5.
    
    Device will never run higher than Frequencies set by user.
    
    Changing frequencies via terminal(Inspired by XDA_BAM)
    
    For changing frequencies,
    
    Eg. echo "1300" > /sys/power/overclock_opp5
    
    The Frequency table is as follows-
    OPP1:  100 -  500 MHz, stock  300 MHz
    OPP2:  501 -  700 MHz, stock  600 MHz
    OPP3:  701 -  900 MHz, stock  800 MHz
    OPP4:  901 - 1100 MHz, stock 1000 MHz
    OPP5: 1101 - 1500 MHz, stock 1200 MHz
    
    Credits: Amit bagaria for overclocking to 1.1Ghz
             XDA_BAM for first overclocking SL above 1.1Ghz

--- arch/arm/mach-omap2/voltage.c
+++ arch/arm/mach-omap2/voltage.c
@@ -364,10 +364,18 @@ static struct omap_volt_data omap34xx_vdd1_volt_data[] = {
 };
 
 static struct omap_volt_data omap36xx_vdd1_volt_data[] = {
+	/*
 	{.volt_nominal = 1025000, .sr_oppmargin = 37500, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1200000, .sr_oppmargin = 37500, .sr_errminlimit = 0xF9, .vp_errgain = 0x16, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1330000, .sr_oppmargin = 37500, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1387500, .sr_oppmargin = 62500,  .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
+	{.volt_nominal = 1420000, .sr_oppmargin = 62500, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
+	*/
+	{.volt_nominal = 1000000, .sr_oppmargin = 37500, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 1162500, .sr_oppmargin = 37500, .sr_errminlimit = 0xF9, .vp_errgain = 0x16, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 1300000, .sr_oppmargin = 37500, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 1350000, .sr_oppmargin = 62500, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
+	{.volt_nominal = 1400000, .sr_oppmargin = 62500, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
 };
 
 /* VDD2 */
@@ -2539,7 +2547,7 @@ int omap_voltage_scale(struct voltagedomain *voltdm)
 			printk("[DEBUG] curr_volt =%d ,volt = %d  \n ", curr_volt,volt);
 #endif 
 
-	
+
 
 	/* Disable smartreflex module across voltage and frequency scaling */
 	if (curr_volt != volt) {

4e34fc6f21935cd062ca91a8d3f839dee7f61052
AdiPat <adithemagnificent@gmail.com>
2012-04-15 18:27:32 +0530

    Revert:lowmemorykiller tweak, fixed error in sched_debug, Updated Defconfig.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -36,8 +36,6 @@
 #include <linux/sched.h>
 #include <linux/notifier.h>
 
-#define SEC_ADJUST_LMK
-
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
 	0,
@@ -93,13 +91,9 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	int selected_oom_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES);
-   #ifdef SEC_ADJUST_LMK
-	int other_file = global_page_state(NR_INACTIVE_FILE)
-	global_page_state(NR_ACTIVE_FILE);
-   #else 
 	int other_file = global_page_state(NR_FILE_PAGES) -
-	global_page_state(NR_SHMEM);
-   #endif
+						global_page_state(NR_SHMEM);
+
 	/*
 	 * If we already have a death outstanding, then
 	 * bail out right away; indicating to vmscan
@@ -116,22 +110,12 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-#ifdef SEC_ADJUST_LMK
-	if ((other_free + other_file) < lowmem_minfree[1])
-#else
-	  if (other_free < lowmem_minfree[1] &&
-		other_file < lowmem_minfree[1])
-#endif
-	{
-	   min_adj = lowmem_adj[1];
-	   break;
+		if (other_free < lowmem_minfree[i] &&
+		    other_file < lowmem_minfree[i]) {
+			min_adj = lowmem_adj[i];
+			break;
+		}
 	}
-}
-
-#ifdef SEC_ADJUST_LMK
-	if (min_adj == OOM_ADJUST_MAX + 1)
-	  return 0;
-#endif
 	if (nr_to_scan > 0)
 		lowmem_print(3, "lowmem_shrink %d, %x, ofree %d %d, ma %d\n",
 			     nr_to_scan, gfp_mask, other_free, other_file,
@@ -140,10 +124,7 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 		global_page_state(NR_ACTIVE_FILE) +
 		global_page_state(NR_INACTIVE_ANON) +
 		global_page_state(NR_INACTIVE_FILE);
-	#ifdef SEC_ADJUST_LMK
-	if (nr_to_scan <= 0 || min_adj == OOM_ADJUST_MAX + 1)
-	#endif
- {
+	if (nr_to_scan <= 0 || min_adj == OOM_ADJUST_MAX + 1) {
 		lowmem_print(5, "lowmem_shrink %d, %x, return %d\n",
 			     nr_to_scan, gfp_mask, rem);
 		return rem;
@@ -194,10 +175,6 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 		force_sig(SIGKILL, selected);
 		rem -= selected_tasksize;
 	}
-#ifdef SEC_ADJUST_LMK
-	else
-	  rem = -1;
-#endif
 	lowmem_print(4, "lowmem_shrink %d, %x, return %d\n",
 		     nr_to_scan, gfp_mask, rem);
 	read_unlock(&tasklist_lock);
